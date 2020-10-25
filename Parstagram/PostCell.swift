37eb087f253907a3c9592dbea32256c4ac883eca
Aninda13 <50640239+Aninda13@users.noreply.github.com>
2020-10-24 23:19:25 -0400

    Added Image reference and username & caption

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Aninda Halder on 10/24/20.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
